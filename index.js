dfae1c85ce68458adbcebdf8673f457ba1f3232a
Rafal Oldziej <rafal.oldziej1@gmail.com>
2022-06-03 19:56:21 -0400

    commit tree 9090c4753c35999e66e227a58ba43c18e683e4fc

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const removeId = document.getElementById('main'); 
+removeId.remove();
+
+const newHeader = document.createElement('h1');
+
+newHeader.setAttribute("id", "victory");
+
+newHeader.textContent =("is the champion");
